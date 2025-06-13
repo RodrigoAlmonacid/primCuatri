88f3136ebec0aa1c3ef9154282cec4c9e08251fe
Rodrigo.Almonacid <rodrigo.almonacid@est.fi.uncoma.edu.ar>
2025-06-13 15:07:31 -0300

    carpeta recu

---
+++ IPOO/2doParcial/Recuperatorio2doParcial/Clase1.php
@@ -0,0 +1,61 @@
+<?php
+Class clase1{
+    //Atributos
+    private
+    private
+    private
+    private
+
+
+//Metodo constructor
+public function __construct()
+{
+    $this->
+    $this->
+    $this->
+    $this->
+}
+
+//metodos de acceso
+public function getA(){
+    return $this->
+}
+public function setA($){
+    $this-> = 
+}
+
+public function getA(){
+    return $this->
+}
+public function setA($){
+    $this-> = 
+}
+
+public function getA(){
+    return $this->
+}
+public function setA($){
+    $this-> = 
+}
+
+public function getA(){
+    return $this->
+}
+public function setA($){
+    $this-> = 
+}
+
+public function getA(){
+    return $this->
+}
+public function setA($){
+    $this-> = 
+}
+
+//metodo toString
+public function __toString()
+{
+    
+}
+}
+?>
